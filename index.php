cdfae605e273da200c1d5b4383a82f76a55c2ec8
DJDemjhon <demjhon2@gmail.com>
2021-05-03 12:20:33 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+
+    <head>
+
+        <link rel="stylesheet" type="text/css" href="css/css_slidepics.css" />
+
+    </head>
+    <body>
+        <ul class="plugspire_slidepics_motion">
+            <p><span></span></p>
+            <p><span></span></p>
+            <p><span></span></p>
+            <p><span></span></p>
+        </ul>
+
+    </body>
+</html>
